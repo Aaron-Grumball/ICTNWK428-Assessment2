ee6964c0a6d0509984f4006e8e6c30cae10df3be
Aaron Grumball <Aaron.GRUMBALL@student.tafesa.edu.au>
2026-09-12 23:39:16 +0930

    Create initial system administration module

---
+++ system_admin.psm1
@@ -0,0 +1,6 @@
+# ICTNWK428 Assessment 2
+# Windows Server 2022 administration module
+# Author: Aaron Grumball
+#
+# This module contains functions used to remotely configure
+# and manage the Mick And Macks Pies Windows Server environment.
